e73372715c4cd96b8683cb8a1517d7ccece6b62f
Alexandre PONCET <alexandreponcet.mail@gmail.com>
2016-11-20 21:31:01 +0100

    Add reducer connection with booklist .

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class BookList extends Component{
   renderList() {
@@ -11,11 +12,18 @@ class BookList extends Component{
 
   render() {
     return(
-        <ul className="list-group col-sm-4" >
+        <ul className="list-group col-sm-4">
           {this.renderList()}
         </ul>
     );
   }
 };
 
-export default BookList;
+//Connection between redux and react.
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
